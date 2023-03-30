d22aad99296804f37070967687f05a1b15dd69cc
Adrian Baltag <fbaltag83@gmail.com>
2023-03-30 17:14:49 -0400

    week1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,169 @@
-import React from 'react';
-import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { Navigation } from "./components/Navigation";
+// import { PokemonCard } from "./components/PokemonCard";
+// import axios from "axios";
+
+// const LIMIT = 150;
+// const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+
+// function App() {
+//   const [pokemon, setPokemon] = useState([]);
+
+//   useEffect(() => {
+//     fetch(pokeApi)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setPokemon(data.results);
+//       })
+//       .catch((error) => {
+//         console.log("Pokemon not found", error);
+//       });
+//   }, []);
+//   return (
+//     <div data-testid="app">
+//       <Navigation />
+
+//       <h1>Pokemon should appear here</h1>
+//       {pokemon.map((pokemon) => (
+//         <PokemonCard key={pokemon.name} url={pokemon.url} name={pokemon.name} />
+//       ))}
+//       <ul>
+//         {pokemon.abilities.map((ability) => (
+//           <li key={ability.ability.name}>{ability.ability.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// import React, { useState, useEffect } from "react";
+// import { Navigation } from "./components/Navigation";
+// import { PokemonCard } from "./components/PokemonCard";
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import InputGroup from "react-bootstrap/InputGroup";
+// import FormControl from "react-bootstrap/FormControl";
+// import Container from "react-bootstrap/Container";
+
+// const LIMIT = 150;
+// const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+
+// function App() {
+//   const [pokemonList, setPokemonList] = useState([]);
+//   const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+//   const [searchInput, setSearchInput] = useState("");
+
+//   useEffect(() => {
+//     fetch(pokeApi)
+//       .then((res) => res.json())
+//       .then((data) => setPokemonList(data.results))
+//       .catch((error) => console.log("Pokemon not found", error));
+//   }, []);
+
+//   useEffect(() => {
+//     setFilteredPokemonList(
+//       pokemonList.filter((pokemon) =>
+//         pokemon.name.toLowerCase().includes(searchInput.toLowerCase())
+//       )
+//     );
+//   }, [pokemonList, searchInput]);
+
+//   const handleSearchInputChange = (event) => {
+//     setSearchInput(event.target.value);
+//   };
+
+//   return (
+//     <Container fluid>
+//       <Navigation />
+
+//       <Row className="my-5">
+//         <Col>
+//           <InputGroup className="mb-3">
+//             <InputGroup.Text>Search Pokemon</InputGroup.Text>
+//             <FormControl
+//               placeholder="Enter Pokemon Name"
+//               aria-label="Enter Pokemon Name"
+//               value={searchInput}
+//               onChange={handleSearchInputChange}
+//             />
+//           </InputGroup>
+//         </Col>
+//       </Row>
+//       <Row>
+//         {filteredPokemonList.map((pokemon) => (
+//           <Col xs={12} sm={6} md={4} lg={3} key={pokemon.name} className="my-3">
+//             <PokemonCard pokemonName={pokemon.name} />
+//           </Col>
+//         ))}
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export { App };
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./components/Navigation";
+import { PokemonCard } from "./components/PokemonCard";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    fetch(pokeApi)
+      .then((res) => res.json())
+      .then((data) => setPokemonList(data.results))
+      .catch((error) => console.log("Pokemon not found", error));
+  }, []);
+
+  useEffect(() => {
+    setFilteredPokemonList(
+      pokemonList.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchInput.toLowerCase())
+      )
+    );
+  }, [pokemonList, searchInput]);
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
 
   return (
-    <div data-testid="app">
+    <Container className="my-5">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
-    </div>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group>
+              <Form.Label>Search Pokemon</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Pokemon Name"
+                value={searchInput}
+                onChange={handleSearchInputChange}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        {filteredPokemonList.map((pokemon) => (
+          <Col xs={12} sm={6} md={4} lg={4} key={pokemon.name} className="my-3">
+            {/* add shadow to card */}
+            <PokemonCard pokemonName={pokemon.name} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,88 @@
-import React from 'react';
+// import React from "react";
+// import { useState, useEffect } from "react";
 
-function PokemonCard({ url, name }) {
+// function PokemonCard({ url, name }) {
+//   const [pokemon, setPokemon] = React.useState(null);
+
+//   useEffect(() => {
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setPokemon(data);
+//       })
+//       .catch((error) => {
+//         console.log("Pokemon not found", error);
+//       });
+//   }, []);
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <img src={pokemon?.sprites.front_default} alt={name} />
+//       <h3>{abilities}</h3>
+//     </div>
+//   );
+// }
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+
+function PokemonCard(props) {
+  const [pokemonData, setPokemonData] = useState(null);
+  const { pokemonName } = props;
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((res) => res.json())
+      .then((data) => setPokemonData(data))
+      .catch((error) => console.log("Pokemon not found", error));
+  }, [pokemonName]);
+
+  if (!pokemonData) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, sprites, abilities } = pokemonData;
 
   return (
-    <div>
-        pokemon card
-    </div>
+    <Card style={{ width: "18rem" }} bg="dark" text="light" shadow>
+      <Card.Img variant="top" src={sprites.front_default} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text as="div">
+          <ul>
+            {abilities.map((ability) => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
 export { PokemonCard };
+
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+
+// function PokemonCard(props) {
+//   const { pokemonData } = props;
+//   const { name, sprites, abilities } = pokemonData;
+
+//   return (
+//     <Card style={{ width: "18rem" }}>
+//       <Card.Img variant="top" src={sprites.front_default} />
+//       <Card.Body>
+//         <Card.Title>{name}</Card.Title>
+//         <Card.Text as="div">
+//           <ul>
+//             {abilities.map((ability) => (
+//               <li key={ability.ability.name}>{ability.ability.name}</li>
+//             ))}
+//           </ul>
+//         </Card.Text>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default PokemonCard;
